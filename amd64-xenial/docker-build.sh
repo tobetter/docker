0260139fb9994d3e1d915d1af5d8598b04e45647
Dongjin Kim <tobetter@gmail.com>
2018-07-24 00:33:18 +0900

    amd64/xenial: modify the default Docker file
    Change-Id: I1608141bdd6ed43090000e16fc71c85eeaeb5ade
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- amd64-xenial/docker-build.sh
+++ amd64-xenial/docker-build.sh
@@ -4,8 +4,7 @@ ARCH=amd64
 DISTRIBUTION=xenial
 
 if [ -z ${DOCKER_TAG} ]; then
-	echo 'DOCKER_TAG' is missing, so using the current date YYYYMMDD
-	DOCKER_TAG=":`date +%Y%m%d`"
+	DOCKER_TAG=":latest"
 else
 	[ ${DOCKER_TAG:0:1} =  ":" ] || DOCKER_TAG=":${DOCKER_TAG}"
 fi
